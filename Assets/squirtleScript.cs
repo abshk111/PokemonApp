af0a59ab191885b051b02747c84aceb92b97b63c
abshk111 <abhishek@hellobuddy.in>
2016-09-11 05:16:14 +0530

    added files

---
+++ Assets/squirtleScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class squirtleScript : MonoBehaviour {
+
+    public Button attackButton1;
+    public GameObject hose;
+
+	// Use this for initialization
+	void Start () {
+        attackButton1.onClick.AddListener (attackButton1Down);
+        hose.transform.FindChild("WaterShower").gameObject.SetActive(false);
+    }
+	
+    IEnumerator Wait()
+    {
+        hose.transform.FindChild("WaterShower").gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        hose.transform.FindChild("WaterShower").gameObject.SetActive(false);
+
+    }
+
+    void attackButton1Down()
+    {
+        StartCoroutine(Wait());
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+ 
